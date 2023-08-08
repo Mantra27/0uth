c40ebc358ba5987e7962c0cae55de2baae6bfa8a
Mantra Gohil <mantragohil@mantra-2.local>
2023-08-08 18:19:34 +0530

    github oauth added & /test removed from repo

--- test/index.ts
+++ test/index.ts
@@ -3,11 +3,49 @@ import zerouth from '../lib/index';
 import "../lib/middleware/_serial-d_serial";
 const express = require("express")
 
+const google = {
+  client: 'google',
+  client_id: '1014276340059-orscm84ijkimm5vp5qkemp1kmjl4cvpe.apps.googleusercontent.com',
+  redirect_url: '/google/callback',
+  client_secret: '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',
+  success_redirect: '/success',
+  failure_redirect: '/login',
+  scope: ["email", "profile"]
+};
+
+const discord = {
+  client: 'discord',
+  client_id: '1138425053659279452',
+  redirect_url: '/auth/dsc',
+  client_secret: 'EFSd6dcuqUbmbobXR8V_v1XOkaDJLsd7',
+  success_redirect: '/discord/success',
+  failure_redirect: '/login',
+  scope: ['identify', 'email', 'guilds', 'guilds.join']
+};
+
+const reddit = {
+  client: 'reddit',
+  client_id: '2d0aOJ2LgNJ1g0Vb7o735Q',
+  redirect_url: '/reddit/callback',
+  client_secret: 'p_74ZvDRIlvJqAsvIRjE_v4iOyaI3g',
+  success_redirect: '/success',
+  failure_redirect: '/login',
+};
+
+const facebook = {
+  client: 'facebook',
+  client_id: '250087077892987',
+  redirect_url: '/facebook/callback',
+  client_secret: '5198bb87eb2c7faae3f409c6bea4e902',
+  success_redirect: '/success',
+  failure_redirect: '/login',
+};
+
 const github = {
   client: 'github',
-  client_id: '250087077892987',
+  client_id: 'Iv1.6125f961d1c5713a',
   redirect_url: '/github/callback',
-  client_secret: '5198bb87eb2c7faae3f409c6bea4e902',
+  client_secret: '3ac3bcf26b1d0508062095f9c281cc621f068e33',
   success_redirect: '/success',
   failure_redirect: '/login',
 };
@@ -25,6 +63,8 @@ app.use(zerouth("/google", google))
 app.use(zerouth("/discord", discord))
 app.use(zerouth("/reddit", reddit))
 app.use(zerouth("/facebook", facebook))
+app.use(zerouth("/github", github))
+
 
 
 app.get("/", (req:any, res:any)=>{
